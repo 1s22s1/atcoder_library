6c5b79fbd5da9b7950e498b225db9622fb66036a
1s22s1 <1s22s1@users.noreply.github.com>
2025-06-04 00:23:28 +0900

    Add maze test

---
+++ Graph.cpp
@@ -0,0 +1,89 @@
+#include <vector>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+class LinearGraph
+{
+
+public:
+    vector<int> visited;
+    vector<int> previous;
+
+    LinearGraph(vector<vector<int>> graph, int start = 0)
+    {
+        visited.assign(graph.size(), -1);
+        previous.assign(graph.size(), -1);
+
+        queue<int> queue;
+        queue.push(start);
+
+        visited.at(start) = 0;
+
+        while (!queue.empty())
+        {
+            int current = queue.front();
+            queue.pop();
+
+            for (int next : graph.at(current))
+            {
+                if (visited.at(next) != -1)
+                {
+                    continue;
+                }
+
+                queue.push(next);
+                visited.at(next) = visited.at(current) + 1;
+                previous.at(next) = current;
+            }
+        }
+    }
+};
+
+class MazeGraph
+{
+public:
+    vector<vector<int>> visited;
+
+    MazeGraph(vector<string> maze, pair<int, int> start)
+    {
+        visited.assign(maze.size(), vector<int>(maze.at(0).size(), -1));
+
+        queue<pair<int, int>> queue;
+        queue.push(start);
+
+        visited.at(start.first).at(start.second) = 0;
+
+        vector<pair<int, int>> moves = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        while (!queue.empty())
+        {
+            auto current = queue.front();
+            queue.pop();
+
+            for (auto move : moves)
+            {
+                int next_x = current.first + move.first;
+                int next_y = current.second + move.second;
+
+                if (next_x < 0 || next_x >= maze.size() || next_y < 0 || next_y >= maze.at(0).size())
+                {
+                    continue;
+                }
+
+                if (maze.at(next_x).at(next_y) == '#')
+                {
+                    continue;
+                }
+
+                if (visited.at(next_x).at(next_y) != -1)
+                {
+                    continue;
+                }
+
+                queue.push({next_x, next_y});
+                visited.at(next_x).at(next_y) = visited.at(current.first).at(current.second) + 1;
+            }
+        }
+    }
+};
--- GraphTest.cpp
+++ GraphTest.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
 
-#include "LinearGraph.cpp"
+#include "Graph.cpp"
 
 using namespace std;
 
-TEST(GraphTest, BasicAssertions)
+TEST(LinearGraphTest, BasicAssertions)
 {
     vector<vector<int>> g(4);
     vector<vector<int>> vec = {{0, 1}, {1, 2}, {2, 3}, {3, 0}};
@@ -22,4 +22,14 @@ TEST(GraphTest, BasicAssertions)
 
     vector<int> expected_previous = {-1, 0, 1, 0};
     EXPECT_EQ(expected_previous, linear_graph.previous);
+}
+
+TEST(MazeGraphTest, BasicAssertions)
+{
+    vector<string> maze = {"...#.", ".#.#.", ".#..."};
+
+    MazeGraph maze_graph(maze, {0, 0});
+
+    vector<vector<int>> expected_visited = {{0, 1, 2, -1, 8}, {1, -1, 3, -1, 7}, {2, -1, 4, 5, 6}};
+    EXPECT_EQ(expected_visited, maze_graph.visited);
 }
