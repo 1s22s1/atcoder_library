92dff520d362a0aad89de544cb742ce06e15a97a
1s22s1 <1s22s1@users.noreply.github.com>
2025-07-18 11:40:30 +0900

    :fire: Remove blank line

--- RLETest.cpp
+++ RLETest.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 TEST(RLETest, BasicAssertions)
 {
-
     vector<pair<char, int>> expected_visited = {{'a', 1}, {'b', 2}, {'c', 3}};
     EXPECT_EQ(expected_visited, rle("abbccc"));
 }
