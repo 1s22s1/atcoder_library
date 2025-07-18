660b31f12730ac0339bca05220e68b1bb7c0190e
1s22s1 <1s22s1@users.noreply.github.com>
2025-07-18 11:33:24 +0900

    Add RLE

---
+++ RLE.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<pair<char, int>> rle(string s)
+{
+    vector<pair<char, int>> vec;
+
+    int i = 0;
+    while (i < s.size())
+    {
+        int j = i;
+        while (j < s.size() && s.at(i) == s.at(j))
+        {
+            j++;
+        }
+
+        vec.push_back({s.at(i), j - i});
+    }
+
+    return vec;
+}
