667449cddf4b8f595d0a064d611c9ee946a6f7fd
1s22s1 <1s22s1@users.noreply.github.com>
2025-08-25 09:18:17 +0900

    Move RLE function

--- Misc.cpp
+++ Misc.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <string>
+
 using namespace std;
 
 #define ll long long
@@ -16,4 +19,24 @@ ll mod(ll a, ll n, ll p)
 
     ll tmp = mod(a, n / 2, p);
     return (tmp * tmp) % p;
+}
+
+vector<pair<char, long long>> rle(string s)
+{
+    vector<pair<char, long long>> vec;
+
+    long long i = 0;
+    while (i < s.size())
+    {
+        long long j = i;
+        while (j < s.size() && s.at(i) == s.at(j))
+        {
+            j++;
+        }
+
+        vec.push_back({s.at(i), j - i});
+        i = j;
+    }
+
+    return vec;
 }
--- test/MiscTest.cpp
+++ test/MiscTest.cpp
@@ -7,4 +7,7 @@ using namespace std;
 TEST(MiscTest, BasicAssertions)
 {
     EXPECT_EQ(0, mod(2, 3, 4));
+
+    vector<pair<char, long long>> expected_visited = {{'a', 1}, {'b', 2}, {'c', 3}};
+    EXPECT_EQ(expected_visited, rle("abbccc"));
 }
